8f691ed9dc4ac6c7499ce57cfb1dae797def382e
antonovmike <antonvma@outlook.com>
2022-10-01 13:35:52 +0500

    yaml removed

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 use dotenv::dotenv;
 
-use yaml_rust::YamlLoader;
-use std::io::Read;
-
 mod db;
 mod photo;
 mod hashing;
@@ -16,13 +13,6 @@ fn rocket() -> _ {
 
 
     hashing::hashing_pswd();
-
-    let mut file = std::fs::File::open("config.example.yaml").expect("Unable to open file");
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)
-        .expect("Unable to read file");
-    let d = YamlLoader::load_from_str(&contents).unwrap();
-    println!("Read YAML string: {:?}", d);
 
     rocket::build()
         .attach(db::stage())
