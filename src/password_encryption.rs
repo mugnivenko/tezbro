196397fc1928f01c135a5c3a2d1090afcec62932
megahomyak <g.megahomyak@gmail.com>
2022-10-05 16:14:17 +0500

    Forgot to add salt

--- src/password_encryption.rs
+++ src/password_encryption.rs
@@ -2,6 +2,6 @@ use sha2::{Sha256, Digest};
 
 pub fn encrypt(password: &str) -> Vec<u8> {
     let mut hasher = Sha256::new();
-    hasher.update(password);
+    hasher.update(password.to_owned() + &std::env::var("SALT").unwrap());
     hasher.finalize().to_vec()
 }
